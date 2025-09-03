7eea5e4f01bfb9ac79ee187631386fd543bf32ea
Void <ugonwamadueke2006@gmail.com>
2025-09-03 14:09:08 +0100

    streak_update

--- candaraa_backend/src/helpers/authPassport.js
+++ candaraa_backend/src/helpers/authPassport.js
@@ -18,6 +18,10 @@ passport.use(
         if (!user) return done(null, false, { message: "User not found" });
         const match = await bcrypt.compare(password, user.password);
         if (!match) return done(null, false, { message: "Incorrect password" });
+        const userId = user.id;
+        const streak = user.streak;
+        streak++;
+        await prisma.user.update({ where: { id: userId }, data: {streak: streak} });
         return done(null, user);
       } catch (err) {
         return done(err);
